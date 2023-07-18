5dc0f847f7029ee6e780ad7c8c714718cc5fd1cf
Sean Park <seansangpark@gmail.com>
2023-07-18 10:19:48 -0400

    basic form created with input and select

--- intsureview_fe/src/Central.js
+++ intsureview_fe/src/Central.js
@@ -3,22 +3,25 @@ import React from "react";
 const Central = () => {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a dapibus
-        elit. Quisque vel nibh dolor. Aenean suscipit vitae tellus non
-        porttitor. Nam efficitur ultricies ante, eu hendrerit orci vulputate
-        ullamcorper. Sed ut dolor consectetur, pulvinar mi tempor, posuere elit.
-        Donec sit amet nisi lobortis, consequat tellus quis, lobortis risus.
-        Aliquam erat volutpat. Maecenas sit amet purus viverra, porta risus sed,
-        euismod erat. Fusce venenatis consequat consequat. Fusce ipsum arcu,
-        scelerisque at sagittis id, pulvinar in nisi. Aliquam placerat, est quis
-        semper rutrum, lorem dui condimentum arcu, et ornare ipsum sapien et
-        enim. Phasellus convallis, enim euismod blandit interdum, mauris lacus
-        facilisis libero, eget ultricies eros est aliquet massa. Phasellus vel
-        lacus ligula. Nam volutpat dui nec ipsum molestie, sit amet tempor nibh
-        mattis. Integer pharetra volutpat pharetra. Morbi vestibulum aliquet
-        dolor, quis ornare lacus.
-      </p>
+      <h2>Central Form</h2>
+      <form>
+        <label>
+          Text Input
+          <input
+            type="text"
+            placeholder="Enter an input"
+            required
+          />
+        </label>
+        <label>
+          Yes or No
+          <select required>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
