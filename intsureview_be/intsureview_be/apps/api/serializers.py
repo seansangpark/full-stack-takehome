1a369d59a307ed860217fb19df4967159749a02b
Sean Park <seansangpark@gmail.com>
2023-07-18 20:29:46 -0400

    designing backend

--- intsureview_be/intsureview_be/apps/api/serializers.py
+++ intsureview_be/intsureview_be/apps/api/serializers.py
@@ -1,14 +1,9 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from intsureview_be.apps.api.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "username", "email", "groups"]
+        fields = '__all__'
 
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ["url", "name"]
